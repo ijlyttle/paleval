772d8a1aad2951a85dce244e39e05cc3e9806859
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-04 09:45:17 -0500

    Add tests for cvd

--- tests/testthat/test-color-vision.R
+++ tests/testthat/test-color-vision.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+name <- "Purple-Green"
+n <-  11
+fcont <- pev_fcont(name)
+fdisc <- pev_fdisc(fcont, n = n)
+
+test_that("cvd modification works", {
+
+  # discrete
+  expect_identical(
+    pev_fpal_cvd(fdisc, type = "deutan")(), # get all colors
+    colorspace::deutan(colorspace::diverging_hcl(n = n, palette = name))
+  )
+
+  # continuous
+  expect_identical(
+    pev_fdisc(pev_fpal_cvd(fcont, type = "deutan"), n = n)(),
+    colorspace::deutan(colorspace::diverging_hcl(n = n, palette = name))
+  )
 })
