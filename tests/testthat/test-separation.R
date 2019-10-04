d80a59c737f766e738a3bb61dc4bf16aa0961816
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-04 18:26:17 -0500

    Increase coverage

--- tests/testthat/test-separation.R
+++ tests/testthat/test-separation.R
@@ -2,6 +2,8 @@ library("colorspace")
 
 good <- c("#000000", "#FFFFFF")
 test_good <- pev_data_separation(good, method = "cie2000")
+test_good_none <-
+  pev_data_separation(good, method = "cie2000", include_cvd = FALSE)
 plot_good <- pev_gg_separation(test_good)
 
 test_that("separation data works", {
@@ -18,6 +20,14 @@ test_that("separation data works", {
     c(0, 100, 100, 0, 0, 99.75180, 99.75180, 0, 0, 99.97655, 99.97655, 0, 0, 100, 100, 0),
     tolerance = 1.e-5
   )
+
+  expect_equal(
+    test_good_none$distance,
+    c(0, 100, 100, 0),
+    tolerance = 1.e-5
+  )
+
+
 
 })
 
