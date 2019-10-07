3dc0f4761709ce4cdf2f7df99513ed07818f78d0
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-06 23:03:08 -0500

    Add test

--- tests/testthat/test-derivative.R
+++ tests/testthat/test-derivative.R
@@ -1,4 +1,7 @@
 data_drv <- pev_data_derivative(pev_fcont("Purple-Green"))
+data_drv_none <-
+  pev_data_derivative(pev_fcont("Purple-Green"), include_cvd = FALSE)
+
 g <- pev_gg_derivative(data_drv)
 
 test_that("hex-derivative works", {
@@ -8,6 +11,15 @@ test_that("hex-derivative works", {
   expect_identical(
     names(data_drv),
     c("cvd", "x", "hex", "d_distance_d_x")
+  )
+
+  expect_identical(
+    unique(data_drv$cvd),
+    c("none", "deutan", "protan", "tritan")
+  )
+  expect_identical(
+    unique(data_drv_none$cvd),
+    "none"
   )
 
 })
